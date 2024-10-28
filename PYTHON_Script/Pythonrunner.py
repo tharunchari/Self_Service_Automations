aa4ab8910bc35347bae626445589eacfeeb35a75
Saikiran Vallabhuni <svallabhuni@vitechinc.com>
2024-10-28 04:16:54 -0400

    Update Pythonrunner.py

--- PYTHON_Script/Pythonrunner.py
+++ PYTHON_Script/Pythonrunner.py
@@ -17,63 +17,78 @@ def send_sns_notification(subject, message):
     except Exception as e:
         print("Failed to send SNS notification:", str(e))
  
-def mark_instance_as_notified(instance_id):
-    # Add a 'Notified' tag to the instance after sending the notification
-    ec2_client = boto3.client('ec2')
-    try:
-        ec2_client.create_tags(Resources=[instance_id], Tags=[{'Key': 'Notified', 'Value': 'True'}])
-        print(f"Instance {instance_id} marked as notified")
-    except Exception as e:
-        print(f"Failed to tag instance {instance_id}: {str(e)}")
- 
 def check_running_ec2_instances():
     # Create a Boto3 EC2 client
     ec2_client = boto3.client('ec2')
-    # Define the tag key and value to filter instances
+    # Define the tag keys and values to filter instances for each runner type
     tag_key = 'Name'
-    tag_value = 'Github_Self_Hosted_Runner'
+    self_hosted_runner_value = 'Github_Self_Hosted_Runner'
+    bam_ip_value = 'bam::ip*'  # Pattern to match instance names starting with 'bam::ip'
     # Get the current time in UTC
     current_time = datetime.now(timezone.utc)
-    # List to accumulate instances running for more than 2 hours
-    instances_to_notify = []
-    # Retrieve instance descriptions based on the tag filter
-    response = ec2_client.describe_instances(Filters=[
-        {'Name': f'tag:{tag_key}', 'Values': [tag_value]},
+    # Lists to accumulate instances running for each type
+    self_hosted_instances = []
+    bam_ip_instances = []
+ 
+    # Retrieve instances for self-hosted runners running for more than 2 hours
+    response_self_hosted = ec2_client.describe_instances(Filters=[
+        {'Name': f'tag:{tag_key}', 'Values': [self_hosted_runner_value]},
         {'Name': 'instance-state-name', 'Values': ['running']}
     ])
- 
-    # Extract instance details
-    for reservation in response['Reservations']:
+    # Extract self-hosted runners running more than 2 hours
+    for reservation in response_self_hosted['Reservations']:
         for instance in reservation['Instances']:
             instance_id = instance['InstanceId']
             launch_time = instance['LaunchTime']
-            # Check if instance has already been notified
-            notified_tag = next((tag['Value'] for tag in instance.get('Tags', []) if tag['Key'] == 'Notified'), None)
-            # Calculate the running time
             running_time = current_time - launch_time
-            # If running for more than 2 hours and not yet notified
-            if running_time.total_seconds() > 7200 and notified_tag != 'True':
-                instance_details = {
+            if running_time.total_seconds() > 300:  # 2 hours = 7200 seconds
+                self_hosted_instances.append({
                     'Instance ID': instance_id,
                     'Launch Time': launch_time,
                     'Running Time': running_time
-                }
-                instances_to_notify.append(instance_details)
-                mark_instance_as_notified(instance_id)
+                })
  
-    # Prepare email subject and body
-    subject = "EC2 Github_Self_Hosted_Runner Instances Running for More Than 2 hours"
-    message = ""
-    for instance in instances_to_notify:
-        message += f"Instance ID: {instance['Instance ID']}\n"
-        message += f"Launch Time: {instance['Launch Time']}\n"
-        message += f"Running Time: {instance['Running Time']}\n\n"
+    # Retrieve instances for bam::ip* pattern running for more than 3 hours
+    response_bam_ip = ec2_client.describe_instances(Filters=[
+        {'Name': f'tag:{tag_key}', 'Values': [bam_ip_value]},
+        {'Name': 'instance-state-name', 'Values': ['running']}
+    ])
+    # Extract bam::ip* instances running more than 3 hours
+    for reservation in response_bam_ip['Reservations']:
+        for instance in reservation['Instances']:
+            instance_id = instance['InstanceId']
+            launch_time = instance['LaunchTime']
+            running_time = current_time - launch_time
+            if running_time.total_seconds() > 300:  # 3 hours = 10800 seconds
+                bam_ip_instances.append({
+                    'Instance ID': instance_id,
+                    'Launch Time': launch_time,
+                    'Running Time': running_time
+                })
  
-    # Send SNS notification if there are instances to notify
-    if instances_to_notify:
-        send_sns_notification(subject, message)
+    # Self-Hosted Runners
+    if self_hosted_instances:
+        subject_self_hosted = "Self-Hosted Github Runners Running for More Than 2 Hours"
+        message_self_hosted = "The following self-hosted Github runners have been running for more than 2 hours:\n"
+        for instance in self_hosted_instances:
+            message_self_hosted += f"Instance ID: {instance['Instance ID']}\n"
+            message_self_hosted += f"Launch Time: {instance['Launch Time']}\n"
+            message_self_hosted += f"Running Time: {instance['Running Time']}\n\n"
+        send_sns_notification(subject_self_hosted, message_self_hosted)
     else:
-        print("No instances running for more than 2 hours.")
+        print("No self-hosted runners running for more than 2 hours.")
  
-# Run the function to check running EC2 instances and send notifications
+    # bam::ip* Instances
+    if bam_ip_instances:
+        subject_bam_ip = "Bamboo Elastic Instances Running for More Than 3 Hours"
+        message_bam_ip = "The following Bamboo Elastic Instances have been running for more than 3 hours:\n"
+        for instance in bam_ip_instances:
+            message_bam_ip += f"Instance ID: {instance['Instance ID']}\n"
+            message_bam_ip += f"Launch Time: {instance['Launch Time']}\n"
+            message_bam_ip += f"Running Time: {instance['Running Time']}\n\n"
+        send_sns_notification(subject_bam_ip, message_bam_ip)
+    else:
+        print("No bam::ip* instances running for more than 3 hours.")
+ 
+# Run the function to check running EC2 instances and send SNS notifications
 check_running_ec2_instances()
