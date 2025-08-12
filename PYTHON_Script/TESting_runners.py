88a4a5cc16802880e4cd85abdc6ab828d47ae724
Saikiran Vallabhuni <svallabhuni@vitechinc.com>
2025-08-12 04:18:39 -0400

    Update TESting_runners.py

--- PYTHON_Script/TESting_runners.py
+++ PYTHON_Script/TESting_runners.py
@@ -14,7 +14,7 @@ ORG_NAME = "vitechsystems"
 GITHUB_API_URL = f"https://api.github.com/orgs/{ORG_NAME}/actions/runners"
  
 # Condition: 7200 seconds = 2 hours
-MIN_RUNNING_SECONDS = 700
+MIN_RUNNING_SECONDS = 100
  
  
 def get_ec2_instances():
